abfad07a851b6b0c284de40ced37826a0af58131
ndrvndr <andreavindra37@gmail.com>
2023-09-13 14:20:42 +0700

    feat(auth): add authentication using clerk

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main>X Clone</main>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
 }
