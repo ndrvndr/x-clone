e786b70994c8c1abce729022fffee0826d4e739c
ndrvndr <andreavindra37@gmail.com>
2023-09-14 14:10:19 +0700

    feat: create onboarding form using shadcn-ui

---
+++ src/lib/models/tweet.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const threadSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    type: String,
+  },
+  children: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+});
+
+const Tweet = mongoose.models.Thread || mongoose.model("Twit", threadSchema);
+
+export default Tweet;
