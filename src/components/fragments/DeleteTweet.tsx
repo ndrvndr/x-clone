6a54d23bf6c098590387f12c22af0115bfd99c69
ndrvndr <andreavindra37@gmail.com>
2023-09-15 11:23:33 +0700

    feat: add delete tweet feature

--- src/components/fragments/DeleteTweet.tsx
+++ src/components/fragments/DeleteTweet.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { deleteTweet } from "@/lib/actions/tweet.actions";
-import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { RiDeleteBin7Fill } from "react-icons/ri";
 
